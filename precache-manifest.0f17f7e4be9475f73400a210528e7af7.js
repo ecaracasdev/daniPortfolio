a04ad9370bef5c002bfcadddcd8d7c8c83f33e56
elias caracas <elias.caracas@eggcooperation.com>
2022-10-28 20:07:01 -0300

    Updates

---
+++ precache-manifest.0f17f7e4be9475f73400a210528e7af7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ed98beeb72ce3c61ba2",
+    "url": "/daniPortfolio/static/css/main.6a9972ba.chunk.css"
+  },
+  {
+    "revision": "2ed98beeb72ce3c61ba2",
+    "url": "/daniPortfolio/static/js/main.2ed98bee.chunk.js"
+  },
+  {
+    "revision": "d4634b5ed7ed2f72990d",
+    "url": "/daniPortfolio/static/js/1.d4634b5e.chunk.js"
+  },
+  {
+    "revision": "c9cc8eacf105c51969e1",
+    "url": "/daniPortfolio/static/js/runtime~main.c9cc8eac.js"
+  },
+  {
+    "revision": "0eeb35b789f3952d56ec2ab79f2863c3",
+    "url": "/daniPortfolio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/daniPortfolio/precache-manifest.0f17f7e4be9475f73400a210528e7af7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/daniPortfolio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
